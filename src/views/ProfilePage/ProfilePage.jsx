4c43b68422748ce0b5f1d1acb2a45c1b1acd9fa9
andy <rygh4775@gmail.com>
2019-05-08 23:58:23 +0900

    add: 그룹 목록 제공 기능 추가 (마이 페이지)

--- src/views/ProfilePage/ProfilePage.jsx
+++ src/views/ProfilePage/ProfilePage.jsx
@@ -3,10 +3,6 @@ import React from "react";
 import classNames from "classnames";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
-// @material-ui/icons
-import Camera from "@material-ui/icons/Camera";
-import Palette from "@material-ui/icons/Palette";
-import Favorite from "@material-ui/icons/Favorite";
 // core components
 import Header from "components/Header/Header.jsx";
 import Footer from "components/Footer/Footer.jsx";
@@ -14,21 +10,12 @@ import Button from "components/CustomButtons/Button.jsx";
 import GridContainer from "components/Grid/GridContainer.jsx";
 import GridItem from "components/Grid/GridItem.jsx";
 import HeaderLinks from "components/Header/HeaderLinks.jsx";
-import NavPills from "components/NavPills/NavPills.jsx";
 import Parallax from "components/Parallax/Parallax.jsx";
+import Card from "components/Card/Card.jsx";
+import CardBody from "components/Card/CardBody.jsx";
 
 import profile from "assets/img/faces/christian.jpg";
-
-import studio1 from "assets/img/examples/studio-1.jpg";
-import studio2 from "assets/img/examples/studio-2.jpg";
-import studio3 from "assets/img/examples/studio-3.jpg";
-import studio4 from "assets/img/examples/studio-4.jpg";
-import studio5 from "assets/img/examples/studio-5.jpg";
-import work1 from "assets/img/examples/olu-eletu.jpg";
-import work2 from "assets/img/examples/clem-onojeghuo.jpg";
-import work3 from "assets/img/examples/cynthia-del-rio.jpg";
-import work4 from "assets/img/examples/mariya-georgieva.jpg";
-import work5 from "assets/img/examples/clem-onojegaw.jpg";
+import javacafeLogo from "assets/img/javacafe-logo.png";
 
 import profilePageStyle from "assets/jss/material-kit-react/views/profilePage.jsx";
 
@@ -38,14 +25,14 @@ class ProfilePage extends React.Component {
     const imageClasses = classNames(
       classes.imgRaised,
       classes.imgRoundedCircle,
-      classes.imgFluid
+      classes.imgFluid,
+      classes.imgCardTop
     );
-    const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
     return (
       <div>
         <Header
           color="transparent"
-          brand="Material Kit React"
+          brand="CHAINITY"
           rightLinks={<HeaderLinks />}
           fixed
           changeColorOnScroll={{
@@ -60,148 +47,87 @@ class ProfilePage extends React.Component {
             <div className={classes.container}>
               <GridContainer justify="center">
                 <GridItem xs={12} sm={12} md={6}>
+                  {/* TODO : 프로필 사진 및 이름 변경 */}
                   <div className={classes.profile}>
                     <div>
                       <img src={profile} alt="..." className={imageClasses} />
                     </div>
                     <div className={classes.name}>
                       <h3 className={classes.title}>Christian Louboutin</h3>
-                      <h6>DESIGNER</h6>
-                      <Button justIcon link className={classes.margin5}>
-                        <i className={"fab fa-twitter"} />
-                      </Button>
-                      <Button justIcon link className={classes.margin5}>
-                        <i className={"fab fa-instagram"} />
-                      </Button>
-                      <Button justIcon link className={classes.margin5}>
-                        <i className={"fab fa-facebook"} />
-                      </Button>
                     </div>
                   </div>
                 </GridItem>
               </GridContainer>
-              <div className={classes.description}>
-                <p>
-                  An artist of considerable range, Chet Faker — the name taken
-                  by Melbourne-raised, Brooklyn-based Nick Murphy — writes,
-                  performs and records all of his own music, giving it a warm,
-                  intimate feel with a solid groove structure.{" "}
-                </p>
+              <div className={classes.title}>
+                <h3>
+                  활동 그룹
+                  <br />
+                  <small>이미 가입하여 활동 중인 그룹 목록입니다.</small>
+                </h3>
               </div>
-              <GridContainer justify="center">
-                <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
-                  <NavPills
-                    alignCenter
-                    color="primary"
-                    tabs={[
-                      {
-                        tabButton: "Studio",
-                        tabIcon: Camera,
-                        tabContent: (
-                          <GridContainer justify="center">
-                            <GridItem xs={12} sm={12} md={4}>
-                              <img
-                                alt="..."
-                                src={studio1}
-                                className={navImageClasses}
-                              />
-                              <img
-                                alt="..."
-                                src={studio2}
-                                className={navImageClasses}
-                              />
-                            </GridItem>
-                            <GridItem xs={12} sm={12} md={4}>
-                              <img
-                                alt="..."
-                                src={studio5}
-                                className={navImageClasses}
-                              />
-                              <img
-                                alt="..."
-                                src={studio4}
-                                className={navImageClasses}
-                              />
-                            </GridItem>
-                          </GridContainer>
-                        )
-                      },
-                      {
-                        tabButton: "Work",
-                        tabIcon: Palette,
-                        tabContent: (
-                          <GridContainer justify="center">
-                            <GridItem xs={12} sm={12} md={4}>
-                              <img
-                                alt="..."
-                                src={work1}
-                                className={navImageClasses}
-                              />
-                              <img
-                                alt="..."
-                                src={work2}
-                                className={navImageClasses}
-                              />
-                              <img
-                                alt="..."
-                                src={work3}
-                                className={navImageClasses}
-                              />
-                            </GridItem>
-                            <GridItem xs={12} sm={12} md={4}>
-                              <img
-                                alt="..."
-                                src={work4}
-                                className={navImageClasses}
-                              />
-                              <img
-                                alt="..."
-                                src={work5}
-                                className={navImageClasses}
-                              />
-                            </GridItem>
-                          </GridContainer>
-                        )
-                      },
-                      {
-                        tabButton: "Favorite",
-                        tabIcon: Favorite,
-                        tabContent: (
-                          <GridContainer justify="center">
-                            <GridItem xs={12} sm={12} md={4}>
-                              <img
-                                alt="..."
-                                src={work4}
-                                className={navImageClasses}
-                              />
-                              <img
-                                alt="..."
-                                src={studio3}
-                                className={navImageClasses}
-                              />
-                            </GridItem>
-                            <GridItem xs={12} sm={12} md={4}>
-                              <img
-                                alt="..."
-                                src={work2}
-                                className={navImageClasses}
-                              />
-                              <img
-                                alt="..."
-                                src={work1}
-                                className={navImageClasses}
-                              />
-                              <img
-                                alt="..."
-                                src={studio1}
-                                className={navImageClasses}
-                              />
-                            </GridItem>
-                          </GridContainer>
-                        )
-                      }
-                    ]}
-                  />
+              <GridContainer>
+                <GridItem xs={12} sm={6} md={4}>
+                  <Card>
+                    <img src={javacafeLogo} alt="..." className={classes.imgCardTop} />
+                    <CardBody>
+                      <h4 className={classes.cardTitle}>자바카페</h4>
+                      <p>자바카페는 개발자들 사이의 네트워크를 구축하여 서로 발전하고 친목을 도모하는 오프라인 커뮤니티 입니다. 위 목적을 위해 오프라인 스터디와 오프 라인 세미나 등 여러 가지 행사를 기획, 진행하고 있습니다.</p>
+                      <Button 
+                        color="success"
+                        onClick={() => {
+                          alert("[클로즈 베타 준비 중] 잠시만 기다려주세요^^");
+                          return false;
+                        }}
+                        // TODO : 오픈 후 위 3라인 삭제 및 아래 2라인 주석 제거
+                        // href="https://javacafe.javacafe.co.kr"
+                        // target="_blank"
+                      >
+                        이동하기
+                      </Button>
+                    </CardBody>
+                  </Card>
+                </GridItem>
+              </GridContainer>
+              <div className={classes.title}>
+                <h3>
+                  추천 그룹
+                  <br/>
+                  <small>A.I가 당신에게 적합한 그룹을 추천합니다.</small>
+                </h3>
+              </div>
+              <GridContainer>
+                <GridItem xs={12} sm={6} md={4}>
+                  <Card>
+                    <img src="https://chainity.net/wp-content/uploads/2018/07/chainity-logo-V5.png" alt="..." className={classes.imgCardTop} />
+                    <CardBody>
+                      <h4 className={classes.cardTitle}>준비중</h4>
+                      <p>A.I 기반의 추천 시스템으로 선별된 그룹을 추천해 드릴 예정입니다. </p>
+                      {/* <p><small className={classes.textMuted}>Last updated 3 mins ago</small></p> */}
+                      <Button color="primary" disabled>가입하기</Button>
+                    </CardBody>
+                  </Card>
+                </GridItem>
+                <GridItem xs={12} sm={6} md={4}>
+                  <Card>
+                    <img src="https://chainity.net/wp-content/uploads/2018/07/chainity-logo-V5.png" alt="..." className={classes.imgCardTop} />
+                    <CardBody>
+                      <h4 className={classes.cardTitle}>준비중</h4>
+                      <p>A.I 기반의 추천 시스템으로 선별된 그룹을 추천해 드릴 예정입니다. </p>
+                      {/* <p><small className={classes.textMuted}>Last updated 3 mins ago</small></p> */}
+                      <Button color="primary" disabled>가입하기</Button>
+                    </CardBody>
+                  </Card>
+                </GridItem>
+                <GridItem xs={12} sm={6} md={4}>
+                  <Card>
+                    <img src="https://chainity.net/wp-content/uploads/2018/07/chainity-logo-V5.png" alt="..." className={classes.imgCardTop} />
+                    <CardBody>
+                      <h4 className={classes.cardTitle}>준비중</h4>
+                      <p>A.I 기반의 추천 시스템으로 선별된 그룹을 추천해 드릴 예정입니다. </p>
+                      {/* <p><small className={classes.textMuted}>Last updated 3 mins ago</small></p> */}
+                      <Button color="primary" disabled>가입하기</Button>
+                    </CardBody>
+                  </Card>
                 </GridItem>
               </GridContainer>
             </div>
