e2457fafd8e3103bf8c0e0efdc1918f078d91601
yooonhona <lucky72nyh@gmail.com>
2019-06-06 15:24:07 +0900

    feat 로컬 개발 proxy 제거 및 API URL 설정 .env로 변경

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
 
+import "config";
+
 import "assets/scss/material-kit-react.scss?v=1.4.0";
 
 import LandingPage from "views/LandingPage/LandingPage.jsx";
